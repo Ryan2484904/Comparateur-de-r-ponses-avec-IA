dd0ae24265bc89dc8a0f95d9c1bbb6d1c5ba1809
Ryan2484904 <wenrei2468@gmail.com>
2026-04-07 19:46:06 -0400

    Update AIQuestion.js

--- AIQuestion.js
+++ AIQuestion.js
@@ -1,3 +1,4 @@
+
 async function envoieQuestion(contenu){
     try{
         const reponse = await fetch('http://localhost:3000/api/generate', {
@@ -11,7 +12,7 @@ async function envoieQuestion(contenu){
         });
 
         const reponseIA = await reponse.json();
-        console.log("Réponse de l'IA : ", reponseIA);
+        console.log("Réponses des l'IA : ", reponseIA);
         return reponseIA.response;
     }
     catch (error) {
